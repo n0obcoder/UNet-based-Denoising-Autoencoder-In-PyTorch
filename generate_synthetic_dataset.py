9434e3066fd57a2af27e28b3d532c3dca97e4696
n0obcoder <31921968+n0obcoder@users.noreply.github.com>
2019-09-10 00:10:53 +0530

    comments added

--- generate_synthetic_dataset.py
+++ generate_synthetic_dataset.py
@@ -11,6 +11,11 @@ def q(text = ''):
     sys.exit()
 
 def degrade_quality(img):
+    '''
+    This function takes in an image (color or grayscale), downsizes it to a
+    randomly chosen size and then resizes it to the original size of the image,
+    degrading the quality of the image in the process.
+    '''
     h, w = img.shape[0], img.shape[1]
     fx=np.random.randint(50,100)/100
     fy=np.random.randint(50,100)/100
@@ -53,30 +58,37 @@ while '' in lines_list:
     lines_list.remove('')
 
 lines_word_list = [str.split(line) for line in lines_list]
-words_list = [planet for sublist in lines_word_list for planet in sublist] 
+words_list = [words for sublist in lines_word_list for words in sublist] 
 
-print('len(words_list): ', len(words_list))
+print('number of words in the txt file: ', len(words_list))
 
-# random font style
+# list of all the font styles
 font_list = [cv2.FONT_HERSHEY_COMPLEX, 
-cv2.FONT_HERSHEY_COMPLEX_SMALL,
-cv2.FONT_HERSHEY_DUPLEX,
-cv2.FONT_HERSHEY_PLAIN,
-cv2.FONT_HERSHEY_SIMPLEX,
-cv2.FONT_HERSHEY_TRIPLEX,
-cv2.FONT_ITALIC] # cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, cv2.FONT_HERSHEY_SCRIPT_COMPLEX, cursive
+             cv2.FONT_HERSHEY_COMPLEX_SMALL,
+             cv2.FONT_HERSHEY_DUPLEX,
+             cv2.FONT_HERSHEY_PLAIN,
+             cv2.FONT_HERSHEY_SIMPLEX,
+             cv2.FONT_HERSHEY_TRIPLEX,
+             cv2.FONT_ITALIC] # cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, cv2.FONT_HERSHEY_SCRIPT_COMPLEX, cursive
 
-h, w = 64*4, 256*4 # size of synthetic images
+# size of the synthetic images to be generated
+syn_h, syn_w = 64, 256
+
+# scale factor
+scale_h, scale_w = 4, 4
+
+# initial size of the image, scaled up by the factor of scale_h and scale_w
+h, w = syn_h*scale_h, syn_w*scale_w 
 
 word_count = 0
-num_imgs = 35000
+num_imgs = 35000 # max number of synthetic images to be generated
 train_num = int(num_imgs*0.8) # training percent
 print('num_imgs : ', num_imgs)
 print('train_num: ', train_num)
 
 img_count = 1
-word_start_x = 5
-word_end_y = 5
+word_start_x = 5 # min space left on the left side of the printed text
+word_end_y = 5   # min space left on the bottom side of the printed text
 for i in tqdm(range(num_imgs)):
     # make a blank image
     img = np.ones((h, w), dtype = np.uint8)*255
@@ -96,33 +108,32 @@ for i in tqdm(range(num_imgs)):
         word_count += 1
     print_text = print_text[:-1] # to get rif of the last space
 
-    # writing the text on the image
+    # writing the text (in UPPERCASE) on the image
     cv2.putText(img, print_text.upper(), bottomLeftCornerOfText, font, fontScale, fontColor, lineType)
+    
+    # adding noise (horizontal and vertical lines) on the image containing text
     noisy_img = img.copy()
-    #################################################
-    # add horizontal line at the bottom of the text
 
+    ### add horizontal line at the bottom of the text
     black_coords = np.where(noisy_img == fontColor)
-
+    # finding the extremes of the printed text
     ymin = np.min(black_coords[0])
     ymax = np.max(black_coords[0])
     xmin = np.min(black_coords[1])
     xmax = np.max(black_coords[1])
 
-    h_start_x = 0 #np.random.randint(0,xmin)
-    h_end_x   = np.random.randint(int(img.shape[1]*0.8), img.shape[1])
+    h_start_x = 0 #np.random.randint(0,xmin)                           # min x of the horizontal line
+    h_end_x   = np.random.randint(int(img.shape[1]*0.8), img.shape[1]) # max x of the horizontal line
     h_length = h_end_x - h_start_x + 1
-    num_h_lines = np.random.randint(10,30)
+    num_h_lines = np.random.randint(10,30) # partitions to be made in the horizontal line (necessary to make it look like naturally broken lines)
     h_lines = []
+    
     h_start_temp = h_start_x
-    # print('num_h_lines: ', num_h_lines)
-
     next_line = True
     num_line = 0
     while (next_line) and (num_line < num_h_lines):
         if h_start_temp < h_end_x:
             h_end_temp = np.random.randint(h_start_temp + 1, h_end_x + 1)
-            # print('h_end_temp  : ', h_end_temp)
             if h_end_temp < h_end_x:
                 h_lines.append([h_start_temp, h_end_temp]) 
                 h_start_temp = h_end_temp + 1
@@ -135,7 +146,7 @@ for i in tqdm(range(num_imgs)):
             next_line = False
 
     for h_line in h_lines:
-        col = np.random.choice(['black', 'white'], p = [0.65, 0.35])
+        col = np.random.choice(['black', 'white'], p = [0.65, 0.35]) # probabilities of line segment being a solid one or a broken one
         if col == 'black':
             x_points = list(range(h_line[0], h_line[1] + 1))
             x_points_black_prob = np.random.choice([0,1], size = len(x_points), p = [0.2, 0.8])
@@ -144,7 +155,7 @@ for i in tqdm(range(num_imgs)):
                 if x_points_black_prob[idx]:
                     noisy_img[ ymax - np.random.randint(4): ymax + np.random.randint(4), x] = np.random.randint(0,30)  
 
-    # adding vertical lines
+    ### adding vertical lines
     vertical_bool = {'left': np.random.choice([0,1], p =[0.2, 0.8]), 'right': np.random.choice([0,1])} # [1 or 0, 1 or 0] whether to make vertical left line on left and right side of the image
     for left_right, bool_ in vertical_bool.items():
         if bool_:
@@ -173,18 +184,18 @@ for i in tqdm(range(num_imgs)):
     img = cv2.dilate(img,kernel,iterations = dilate_iteration)
     noisy_img = cv2.dilate(noisy_img,kernel,iterations = dilate_iteration)
     # '''
-
+    
     img = degrade_quality(img)
     noisy_img = degrade_quality(noisy_img)
 
-    debug_img = np.ones((2*h, w), dtype = np.uint8)*255
+    debug_img = np.ones((2*h, w), dtype = np.uint8)*255 # to visualize the generated images (clean and noisy)
     debug_img[0:h, :] = img
     debug_img[h:2*h, :] = noisy_img
     cv2.line(debug_img, (0, h), (debug_img.shape[1], h), 150, 5)
 
-    img       = cv2.resize(img,       (0,0), fx = 0.25, fy = 0.25)
-    noisy_img = cv2.resize(noisy_img, (0,0), fx = 0.25, fy = 0.25)
-    debug_img = cv2.resize(debug_img, (0,0), fx = 0.25, fy = 0.25)
+    img       = cv2.resize(img,       (0,0), fx = 1/scale_w, fy = /scale_h)
+    noisy_img = cv2.resize(noisy_img, (0,0), fx = 1/scale_w, fy = /scale_h)
+    debug_img = cv2.resize(debug_img, (0,0), fx = 1/scale_w, fy = /scale_h)
 
     if img_count <= train_num:            
         cv2.imwrite(os.path.join(data_dir, 'train', 'imgs', '{}.jpg'.format(str(img_count).zfill(6))), img) 
