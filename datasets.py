80436484393dd4c84d9a4998a8b2692401118397
n0obcoder <31921968+n0obcoder@users.noreply.github.com>
2019-09-15 11:28:28 +0530

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -20,14 +20,10 @@ class DAE_dataset(Dataset):
         img       = 255 - cv2.imread(self.imgs_data[index] ,0)
         noisy_img = 255 - cv2.imread(self.noisy_imgs_data[index] ,0)
     
-        # padding the images
-        # img = np.pad(img, ((96, 96), (0,0)), constant_values=(0,0))    
-        # noisy_img = np.pad(noisy_img, ((96, 96), (0,0)), constant_values=(0,0))    
-    
         if self.transform is not None:            
             img = self.transform(img)             
             noisy_img = self.transform(noisy_img)  
-        
+
         return img, noisy_img
 
     def __len__(self):
@@ -35,6 +31,7 @@ class DAE_dataset(Dataset):
     
 class custom_test_dataset(Dataset):
     def __init__(self, data_dir, transform = None, out_size = (64, 256)):
+        assert out_size[0] <= out_size[1], 'height/width of the output image shouldn\'t not be greater than 1'
         self.data_dir = data_dir
         self.transform = transform
         self.out_size = out_size
@@ -49,31 +46,32 @@ class custom_test_dataset(Dataset):
     def __getitem__(self, index):  
         # read images in grayscale, then invert them
         img       = 255 - cv2.imread(self.imgs_data[index] ,0)
+                
+        # check if img height exceeds out_size height
+        if img.shape[0] > self.out_size[0]:
+            resize_factor = self.out_size[0]/img.shape[0]
+            img = cv2.resize(img, (0, 0), fx=resize_factor, fy=resize_factor)
 
-        # aspect ratio of the image required to be fed into the model (height/width)
-        out_ar = self.out_size[0]/self.out_size[1]
-
-        # aspect ratio of the image read
-        ar = img.shape[0]/img.shape[1] # heigth/width
+        # check if img width exceeds out_size width
+        if img.shape[1] > self.out_size[1]:
+            resize_factor = self.out_size[1]/img.shape[1]
+            img = cv2.resize(img, (0, 0), fx=resize_factor, fy=resize_factor)
+       
+        # add padding where required
+        # pad height
+        pad_height = self.out_size[0] - img.shape[0]
+        pad_top = int(pad_height/2)
+        pad_bottom = self.out_size[0] - img.shape[0] - pad_top
+        # pad width
+        pad_width = self.out_size[1] - img.shape[1]
+        pad_left = int(pad_width/2)
+        pad_right = self.out_size[1] - img.shape[1] - pad_left
         
-        # aspect ratio is the padding criteria here         
-        if ar >= out_ar:
-            # pad width
-            pad = int(img.shape[0]/out_ar) - img.shape[1]
-            pad1 = int(pad/2)
-            pad2 = int(img.shape[0]/out_ar) - img.shape[1] - pad1
-            img = np.pad(img, ((0,0),(pad1, pad2)), constant_values=(0,0))
-            img = cv2.resize(img, (self.out_size[1], self.out_size[0]))
-        else:
-            # pad height
-            pad = int(img.shape[1]*out_ar) - img.shape[0]
-            pad1 = int(pad/2)
-            pad2 = int(img.shape[1]*out_ar) - img.shape[0] - pad1
-            img = np.pad(img, ((pad1, pad2), (0,0)), constant_values=(0,0))
-            img = cv2.resize(img, (self.out_size[1], self.out_size[0]))
+        img = np.pad(img, ((pad_top, pad_bottom), (pad_left, pad_right)), constant_values=(0,0))    
         
         if self.transform is not None:            
-            img = self.transform(img)             
+            img = self.transform(img)      
+        
         return img
 
     def __len__(self):
